31c1eb4d850403a58d9ba4c3f7bd57a59d7485b7
Chance Zibolski <chance.zibolski@gmail.com>
2014-07-26 14:41:26 -0700

    Add playground package
    Interfaces with play.golang.org

---
+++ playground/playground.go
@@ -0,0 +1,35 @@
+package playground
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+const (
+	shareUrl    = "http://play.golang.org/share"
+	contentType = "application/x-www-form-urlencoded; charset=UTF-8"
+	PlayUrl     = "http://play.golang.org/p/"
+)
+
+func SendToPlayground(content *string) (string, error) {
+	buf := strings.NewReader(*content)
+	resp, err := http.Post(shareUrl, contentType, buf)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func GetPlayUrl(content *string) (string, error) {
+	shareId, err := SendToPlayground(content)
+	if err != nil {
+		return "", err
+	}
+	return PlayUrl + shareId, nil
+}
